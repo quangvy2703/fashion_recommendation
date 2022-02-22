e641a37108ec081a60678a971815d5858e63b225
quangvy2703 <quangvy2703@gmail.com>
2022-02-22 22:10:11 +0700

    update config file

--- encoder_model.py
+++ encoder_model.py
@@ -19,6 +19,7 @@ class TransactionsEncoder(nn.Module):
     def forward(self, input, hidden):
         embedded = self.embedding(input).view(1, 1, -1)
         output = embedded
+        print(output.size())
         output, hidden = self.gru(output, hidden)
         return output, hidden
 
