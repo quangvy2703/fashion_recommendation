97fb283ba4f0bf58ee742c04fad0f038a233dcd9
quangvy2703 <quangvy2703@gmail.com>
2022-02-22 15:31:59 +0700

    update config file

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 cfg = {}
-cfg.ARTICLES_PATH = "datasets/articles.csv"
-cfg.TRANSACTIONS_PATH = "datasets/transactions_train.csv"
-cfg.CUSTOMERS_PATH = "datasets/customers.csv"
-cfg.DATA_DIR = "datasets"
+cfg["ARTICLES_PATH"] = "datasets/articles.csv"
+cfg["TRANSACTIONS_PATH"] = "datasets/transactions_train.csv"
+cfg["CUSTOMERS_PATH"] = "datasets/customers.csv"
+cfg["DATA_DIR"] = "datasets"
 
-cfg.MIN_SEQUENCE_LENGTH = 2
-cfg.MAX_SEQUENCE_LENGTH = 10
-cfg.PERIODS = 7
-cfg.AGE_FILL_VALUE = 36
-cfg.BATCH_SIZE = 16
-cfg.EPOCHS = 100
+cfg["MIN_SEQUENCE_LENGTH"] = 2
+cfg["MAX_SEQUENCE_LENGTH"] = 10
+cfg["PERIODS"] = 7
+cfg["AGE_FILL_VALUE"] = 36
+cfg["BATCH_SIZE"] = 16
+cfg["EPOCHS"] = 100
 
 # MODEL CONFIGS
-cfg.TRANSACTION_ENCODER_INPUT_SIZE = 632
-cfg.CUSTOMER_ENCODER_INPUT_SIZE = 11
-cfg.HIDDEN_SIZE = 1024
-cfg.N_CLASSES = 2
+cfg["TRANSACTION_ENCODER_INPUT_SIZE"] = 632
+cfg["CUSTOMER_ENCODER_INPUT_SIZE"] = 11
+cfg["HIDDEN_SIZE"] = 1024
+cfg["N_CLASSES"] = 2
 
-cfg.LR = 0.01
+cfg["LR"] = 0.01
 
 ARTICLE_FEATURES = ['article_id', 'product_type_no', 'graphical_appearance_no',
                     'colour_group_code', 'perceived_colour_value_id', 'perceived_colour_master_id',
