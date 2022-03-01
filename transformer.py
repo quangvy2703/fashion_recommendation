c9f42b438fae8f46e1447baf371e894c274c0fc5
quangvy2703 <quangvy2703@gmail.com>
2022-03-01 17:26:07 +0700

    transfomer update

--- transformer.py
+++ transformer.py
@@ -168,8 +168,8 @@ def prepare_data(data_dir="datasets_transformer", save_data_dir="saved_dir"):
         training_pairs.append(tensor_from_pair(vocab, source_session, target_session, max_seq_length))
     
     X_train, Y_train = zip(*training_pairs)
-    X_train = torch.transpose(X_train, 1, 0)
-    Y_train = torch.transpose(Y_train, 1, 0)
+    # X_train = torch.transpose(X_train, 1, 0)
+    # Y_train = torch.transpose(Y_train, 1, 0)
     torch.save(X_train, os.path.join(save_data_dir, 'X_train.bin'))
     torch.save(Y_train, os.path.join(save_data_dir, 'Y_train.bin'))
 
@@ -178,8 +178,8 @@ def prepare_data(data_dir="datasets_transformer", save_data_dir="saved_dir"):
         valid_pairs.append(tensor_from_pair(vocab, source_session, target_session, max_seq_length))      
 
     X_valid, Y_valid = zip(*valid_pairs)
-    X_valid = torch.transpose(X_valid, 1, 0)
-    Y_valid = torch.transpose(Y_valid, 1, 0)
+    # X_valid = torch.transpose(X_valid, 1, 0)
+    # Y_valid = torch.transpose(Y_valid, 1, 0)
     torch.save(X_valid, os.path.join(save_data_dir, 'X_valid.bin'))
     torch.save(Y_valid, os.path.join(save_data_dir, 'Y_valid.bin'))
 
