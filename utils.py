645801661eed2cf017176978701aa63b8e466f1d
quangvy2703 <quangvy2703@gmail.com>
2022-03-02 16:37:26 +0700

    transfomer update

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def create_mask(src, tgt):
     tgt_seq_len = tgt.shape[0]
     print("Create mask ", src_seq_len, tgt_seq_len)
     tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
-    src_mask = torch.zeros((src_seq_len, tgt_seq_len), device=DEVICE).type(torch.bool)
+    src_mask = torch.zeros((src_seq_len, src_seq_len), device=DEVICE).type(torch.bool)
 
     src_padding_mask = (src == PAD_IDX).transpose(0, 1)
     tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1)
