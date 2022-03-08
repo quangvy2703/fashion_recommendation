7d52a9a48ca18d28fd42b6abcd0a52598a7b8d4b
quangvy2703 <quangvy2703@gmail.com>
2022-03-09 02:24:45 +0700

    transfomer update

--- transformer.py
+++ transformer.py
@@ -294,9 +294,8 @@ def greedy_decode(model, src, src_features, max_len, start_symbol):
         out = out.transpose(0, 1)
         prob = model.generator(out[:, -1])
         _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.cpu().numpy()
-        print(next_word)
-        ys = torch.cat([ys, torch.tensor(next_word, dtype=torch.long).to(DEVICE)], dim=0)
+        print(next_word.cpu().numpy())
+        ys = torch.cat([ys, [next_word]], dim=0)
         if next_word == EOS_IDX:
             break
     return ys
